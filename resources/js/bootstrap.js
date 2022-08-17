3dc02d045b92d96faf4f954e17b69e300a3b62c0
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-17 06:43:01 +0630

    set up pusher

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+import Echo from "laravel-echo"
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: env('PUSHER_APP_KEY'),
+    cluster: env('PUSHER_APP_CLUSTER'),
+    encrypted: true
+});
